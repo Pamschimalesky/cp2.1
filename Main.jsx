705b0cdab4b4df70bee3258f7f85d78468ec64a7
Pamella Schimalesky Engholm <rm551600@fiap.com.br>
2023-09-14 20:52:56 -0300

    adc main jsx

---
+++ Main.jsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/Index.jsx'
+import EditarProdutos from './routes/EditarProdutos/Index.jsx'
+import Error from './routes/Error/Index.jsx'
