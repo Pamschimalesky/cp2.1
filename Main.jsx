36c740a510c9fc237ede15460ff496111fb38797
Pamella Schimalesky Engholm <rm551600@fiap.com.br>
2023-09-14 20:54:09 -0300

    config rotas

--- Main.jsx
+++ Main.jsx
@@ -3,3 +3,34 @@ import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/Index.jsx'
 import EditarProdutos from './routes/EditarProdutos/Index.jsx'
 import Error from './routes/Error/Index.jsx'
+
+
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App/>,
+      // endereço url errado    
+      errorElement: <Error/>,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        },
+        {
+          path: '/produtos',
+          element: <Produtos/>
+        },
+        {
+          //passar valor para
+          path: '/produtos/editar/:id',
+          element: <EditarProdutos/>
+        },
+        {
+          // endereco certo porem nao existe mais (nao esta habilitado)
+          path: '/antiga',
+          element: <Navigate to='/Home'/>
+        }
+      ] 
+    }
+  ])
